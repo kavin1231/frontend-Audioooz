82683c9652feeee5fa26f1bcd5b8a5f8bcfbd7cf
kavin1231 <chamathkaveendra@gmail.com>
2025-07-22 20:55:45 +0530

    fix

--- audiomart/src/utils/mediaUpload.jsx
+++ audiomart/src/utils/mediaUpload.jsx
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://bahndpmkfvqvhgsucmnq.supabase.co";
+const SUPABASE_URL = "https://unlrjpvwapckhljhjvdt.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhaG5kcG1rZnZxdmhnc3VjbW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NTYxNTUsImV4cCI6MjA2NzUzMjE1NX0.mTcfsEDnUIokVI7l1a4zK64vApYHwI9rq_Fra9KGo38";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubHJqcHZ3YXBja2hsamhqdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTQxNTAsImV4cCI6MjA2ODY3MDE1MH0.ntyBNTw4XYyuXiLcoXd-FRkAxzup2Azohqtl2AT3K8k";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
