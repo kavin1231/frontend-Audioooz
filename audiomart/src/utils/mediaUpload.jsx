0278c9f6e01099314ca9aca425ffc96a8c518dd6
kavin1231 <chamathkaveendra@gmail.com>
2025-07-22 21:05:23 +0530

    update env

--- audiomart/src/utils/mediaUpload.jsx
+++ audiomart/src/utils/mediaUpload.jsx
@@ -1,19 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://unlrjpvwapckhljhjvdt.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubHJqcHZ3YXBja2hsamhqdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTQxNTAsImV4cCI6MjA2ODY3MDE1MH0.ntyBNTw4XYyuXiLcoXd-FRkAxzup2Azohqtl2AT3K8k";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function mediaUpload(file) {
   return new Promise((resolve, reject) => {
     if (file == null) {
-      reject("No file selected");
+      return reject("No file selected");
     }
 
     const timestamp = new Date().getTime();
-    const fileName = timestamp + file.name;
+    const fileName = `${timestamp}-${file.name}`;
 
     supabase.storage
       .from("images")
@@ -21,12 +20,11 @@ export default function mediaUpload(file) {
         cacheControl: "3600",
         upsert: false,
       })
-      .then(() => {
-        const publicUrl = supabase.storage.from("images").getPublicUrl(fileName)
-          .data.publicUrl;
+      .then((res) => {
+        const { publicUrl } = supabase.storage.from("images").getPublicUrl(fileName).data;
         resolve(publicUrl);
       })
-      .catch(() => {
+      .catch((err) => {
         reject("Error uploading file");
       });
   });
